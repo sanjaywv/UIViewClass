3c24c368a1de58f35c36e4d5f87116d92f7ad51c
sanjay singh jadon <sanjay.webvillee@gmail.com>
2020-09-15 21:13:35 +0530

    Framework completed

---
+++ UiViewClass/UiViewClass/UiViewClass.h
@@ -0,0 +1,19 @@
+//
+//  UiViewClass.h
+//  UiViewClass
+//
+//  Created by WV-Mac4 on 15/09/20.
+//  Copyright © 2020 Mac-4. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UiViewClass.
+FOUNDATION_EXPORT double UiViewClassVersionNumber;
+
+//! Project version string for UiViewClass.
+FOUNDATION_EXPORT const unsigned char UiViewClassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UiViewClass/PublicHeader.h>
+
+
